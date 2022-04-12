23c8118ce705bb5ca866062f48b600d7410d7615
JoseArroyave <jarroyave@unal.edu.cogit config --global user.email jarroyave@unal.edu.co>
2022-04-12 01:41:10 -0500

    barra de navegacion y panel de control

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
   $('.btnIngresar').on('click', function (e) {
     e.preventDefault();
   })
+  $('.btnNavbarBuscar').on('click', function (e) {
+    e.preventDefault();
+    var aBuscar = $('#navbarBuscar')[0].value
+    console.log(aBuscar)
+  })
 })
