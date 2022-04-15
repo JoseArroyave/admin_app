51a18496d7ac18c7bb7387dbdc1ee861e131cfa7
JoseArroyave <jarroyave@unal.edu.cogit config --global user.email jarroyave@unal.edu.co>
2022-04-15 00:22:33 -0500

    tabla de precios

--- js/main.js
+++ js/main.js
@@ -7,4 +7,35 @@ $(document).ready(function () {
     var aBuscar = $('#navbarBuscar')[0].value
     console.log(aBuscar)
   })
+
+  // Paginación tabla de precios
+  var table = $("#priceTable").DataTable({
+    language: {
+      paginate: {
+        next: "Siguiente",
+        previous: "Anterior",
+        last: "Último",
+        first: "Primero"
+      },
+      info: "Mostrando _START_ a _END_ de _TOTAL_ de resultados",
+      emptyTable: "No hay registros",
+      infoEmpty: "0 registros",
+      search: "Buscar"
+    },
+    "scrollY": "412px",
+    "paging": true,
+    "responsive": true
+  });
+
+  // Mostrar y ocultar columnas
+  $('a.toggle-vis').on('click', function (e) {
+    e.preventDefault();
+
+    // Get the column API object
+    var column = table.column($(this).attr('data-column'));
+
+    // Toggle the visibility
+    column.visible(!column.visible());
+  });
+
 })
